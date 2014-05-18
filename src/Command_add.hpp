54e9790f98be55ac5febd32b97a8d328b5965bb4
Alex Morton-Cooke <mortoniumcook@gmail.com>
2014-05-18 22:05:50 +0100

    Flag list for git add

---
+++ src/Command_add.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+class Command_add {
+
+public:
+
+private:
+	bool itsDryRunFlag;
+	bool itsVerboseFlag;
+	bool itsForceFlag;
+	// bool itsInteractiveFlag;
+	// bool itsPatchFlag;
+	// bool itsEditFlag;
+	bool itsUpdateFlag;
+	bool itsAllFlag; bool itsNoAllFlag;
+	bool itsIntentToAddFlag;
+	bool itsRefreshFlag;
+	bool itsIgnoreErrorsFlag;
+	bool itsIgnoreMissingFlag;
+
+};
