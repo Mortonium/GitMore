d515eea690aa4633b3cfff195816b1de1f81450e
Alex Morton-Cooke <mortoniumcook@gmail.com>
2014-08-09 17:20:25 +0100

    Basic reference class definition

--- src/git/reference.hpp
+++ src/git/reference.hpp
@@ -9,7 +9,32 @@ namespace git {
 	class reference {
 
 	public:
+		enum class type {
+			branch,
+			note,
+			remote,
+			tag
+		};
+
 		reference();
+		reference(std::string name);
+		virtual ~reference() = 0;
+
+		int compare(const reference& other);
+		int compare(const git_reference* other);
+
+		void rename(std::string new_name);
+
+		void delete_reference();
+
+	protected:
+		git_reference* get_reference();
+
+	private:
+		git_reference* itsReference;
+
+		git_oid itsOID;
+		std::string itsFullName;
 
 	};
 
